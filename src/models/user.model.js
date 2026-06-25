7356667122b2485abdac17de5a5f896176fbc9f0
Zain-The-Coder <zain015976@gmail.com>
2026-06-25 11:52:48 +0500

    made video and user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt' ;
+import jwt from 'jsonwebtoken';
 
 const userSchema = new mongoose.Schema({
     username : {
@@ -42,5 +44,31 @@ const userSchema = new mongoose.Schema({
     } 
 } , {timestamps : true})
 
+userSchema.pre("save" , async function (next) {
+    if(!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password , 10);
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password , this.password)
+};
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+        _id : this._id ,
+        email : this.email ,
+        username : this.username
+    } , process.env.JWT_SECRET_ACCESS ,
+    {expiresIn : "1d"})
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign({
+        _id : this._id
+    } , process.env.JWT_SECRET_REFRESH ,
+    {expiresIn : "7d"})
+}
 
 export const User = mongoose.model("User" , userSchema);
